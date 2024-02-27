8e39cf251b40cffde9caaf36d562745cee3a5e8c
nzaumuimi <nzaumuimi@gmail.com>
2024-02-26 23:20:24 -0800

    binary_trees

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,35 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * print_num - Prints a number
- *
- * @n: Number to be printed
+ * binary_tree_preorder - Performs pre-order traversal on a binary tree.
+ * @tree: Tree to traverse.
+ * @func: Pointer to a function to call for each node.
  */
-void print_num(int n)
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    printf("%d\n", n);
-}
+	if (!tree || !func)
+		return;
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    binary_tree_t *root;
-
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    root->left->left = binary_tree_node(root->left, 6);
-    root->left->right = binary_tree_node(root->left, 56);
-    root->right->left = binary_tree_node(root->right, 256);
-    root->right->right = binary_tree_node(root->right, 512);
-
-    binary_tree_print(root);
-    binary_tree_preorder(root, &print_num);
-    return (0);
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
